b27c9e9cbf3af58af319011b17bff3f0ee7485c8
Alexey <alexey-lopatenko@yandex.ru>
2022-09-02 22:20:59 +0300

    Add origin page-user

--- src/pages/userPage.vue
+++ src/pages/userPage.vue
@@ -1,3 +1,20 @@
 <template>
-  Тут будет страница Личного кабинета пользователя
+  <div class="container-user">
+    <div class="header-user">
+      <div class="header-user-img">
+        <img src="@/assets/images/user.jpg" alt="">
+      </div>
+
+      <div class="header-user-person">
+        <div class="user-person-nickname">Книголюб</div>
+          <div class="user-person-name"><bdi>Александр</bdi></div>
+          <div class="user-person-city"><img src="@/assets/images/icon/ru.gif" alt="" class="user-person-img"> Нижний Новгород, Россия</div>
+          <div class="user-person-descr">Люблю читать книги</div>
+        </div>
+
+        <div class="header-user-badgeinfo"><i>Бейдж инфо</i></div>
+      </div>
+
+      
+    </div>
 </template>
